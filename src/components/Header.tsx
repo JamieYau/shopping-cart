6bbea520ea91eb0c288731932d5ff6855ca5c940
JamieYau <jamie.yau2002@me.com>
2024-01-01 21:20:56 +0000

    Add FontAwesome icons and update header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 // Header.tsx
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.css";
 
 export default function Header() {
@@ -17,6 +19,11 @@ export default function Header() {
             <NavLink to="/store" className={styles.navLink}>
               Store
             </NavLink>
+          </li>
+          <li>
+            <button className={styles.basket} type="button" aria-label="Shopping Basket">
+                <FontAwesomeIcon icon={faCartShopping} />
+            </button>
           </li>
         </ul>
       </nav>
