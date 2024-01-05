8032cc67fb39ffa20c8b4e06d0e659faa08ab847
JamieYau <jamie.yau2002@me.com>
2024-01-05 11:36:11 +0000

    Refactor CSS class names and add filter options

--- src/pages/StorePage/StorePage.tsx
+++ src/pages/StorePage/StorePage.tsx
@@ -7,21 +7,32 @@ export default function StorePage() {
   return (
     <main className={styles.storePage}>
       <div className={styles.sortContainer}>
-        <div>
+        <div className={styles.header}>
           <h2>Products</h2>
           <div>{products.length} results</div>
         </div>
-        <label htmlFor="sort">
-          Sort by:
-          <select name="sort" id="sort">
-            <option value="default">Default</option>
-            <option value="low-high">Price-low to high</option>
-            <option value="high-low">Price-high to low</option>
-            <option value="rating">Rating</option>
-          </select>
-        </label>
+        <div className={styles.actions}>
+          <label htmlFor="filter">
+            Filter by:
+            <select name="filter" id="filter">
+              <option value="all">All</option>
+              <option value="tech">Tech</option>
+              <option value="clothes">Clothes</option>
+              <option value="food">Food</option>
+            </select>
+          </label>
+          <label htmlFor="sort">
+            Sort by:
+            <select name="sort" id="sort">
+              <option value="default">Default</option>
+              <option value="low-high">Price-low to high</option>
+              <option value="high-low">Price-high to low</option>
+              <option value="rating">Rating</option>
+            </select>
+          </label>
+        </div>
       </div>
-      <ProductList products={products} className={styles.store}/>
+      <ProductList products={products} className={styles.store} />
     </main>
   );
 }
