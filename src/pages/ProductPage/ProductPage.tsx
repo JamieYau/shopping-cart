c30812f03c4462dad3315153747640ec393449ea
JamieYau <jamie.yau2002@me.com>
2024-01-11 21:43:36 +0000

    Refactor ProductPage component layout

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -3,6 +3,7 @@ import { useProducts } from "../../contexts/ProductsContext";
 import formatRating from "../../utils/jsxHelpers";
 import { formatPrice } from "../../utils/helpers";
 import { useBasket } from "../../contexts/BasketContext";
+import styles from "./ProductPage.module.css";
 
 export default function ProductPage() {
   const { id } = useParams();
@@ -13,21 +14,25 @@ export default function ProductPage() {
   const { stars, count } = formatRating(product);
 
   return (
-    <main>
-      <h1>{product.title}</h1>
-      <div>{product.category}</div>
-      <img src={product.image} alt={product.title} />
-      <p>{product.description}</p>
-      <div>
-        <p>{formatPrice(product.price)}</p>
-        <div>
-          {stars}
-          <span>{count}</span>
-        </div>
+    <main className={styles.container}>
+      <div className={styles.imageContainer}>
+        <img className={styles.image} src={product.image} alt={product.title} />
       </div>
-      <button type="button" onClick={() => addToBasket(product)}>
-        Add to Cart
-      </button>
+      <div className={styles.details}>
+        <h1>{product.title}</h1>
+        <p className={styles.category}>{product.category}</p>
+        <p className={styles.description}>{product.description}</p>
+        <div>
+          <p>{formatPrice(product.price)}</p>
+          <div className={styles.rating}>
+            {stars}
+            <span>{count}</span>
+          </div>
+        </div>
+        <button type="button" onClick={() => addToBasket(product)}>
+          Add to Cart
+        </button>
+      </div>
     </main>
   );
 }
