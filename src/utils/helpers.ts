0ed0e3fb7c032fb0dee29e85d7bfcb2ecbf0e277
JamieYau <jamie.yau2002@me.com>
2024-01-12 17:54:41 +0000

    Implement sorting and filtering functionality to store page

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { Product } from "../types/types";
+
 export function formatItemCount(count: number): string {
   return `${count} Item${count !== 1 ? "s" : ""}`;
 }
@@ -14,4 +16,30 @@ export function formatPrice(
   });
 
   return formatter.format(price);
+}
+
+export function filterProducts(
+  products: Product[],
+  category: string
+): Product[] {
+  if (category === "all") {
+    return products;
+  }
+  return products.filter((product) => product.category === category);
+}
+
+export function sortProducts(products: Product[], sort: string): Product[] {
+  if (sort === "default") {
+    return products;
+  }
+  if (sort === "price-lowest") {
+    return products.sort((a, b) => a.price - b.price);
+  }
+  if (sort === "price-highest") {
+    return products.sort((a, b) => b.price - a.price);
+  }
+  if (sort === "rating") {
+    return products.sort((a, b) => b.rating.rate - a.rating.rate);
+  }
+  return products;
 }
